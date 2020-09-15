85bdc7bb768e4a99742d7adbedf5e049feb3e504
mnc-bpl-PC\mnc-bpl <anwarulhaque020@gmail.com>
2020-09-15 14:02:30 +0530

    blog integration

--- uvidapp/urls.py
+++ uvidapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    # path('contact-us/', views.contact_us, name="contact_us"),
+    path('blog/', views.blog, name="blog"),
     # path('contactSave',views.contactSave),
     # path('about-us/', views.about_us, name="about_us"),
     # path('services/', views.services, name="services"),
--- uvidapp/views.py
+++ uvidapp/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
+
+def blog(request):
+    return render(request, 'blog.html')
